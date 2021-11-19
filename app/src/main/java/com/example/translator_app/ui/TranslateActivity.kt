0260042e4f91f5d367a1a1a8c8bb655f7ffd78f7
Mykhailo Tkachenko <tk.mykhailo@gmail.com>
2021-11-19 19:34:47 +0100

    Updated layout

--- app/src/main/java/com/example/translator_app/ui/TranslateActivity.kt
+++ app/src/main/java/com/example/translator_app/ui/TranslateActivity.kt
@@ -1,11 +1,12 @@
-package com.example.translator_app
+package com.example.translator_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.translator_app.R
 
-class MainActivity : AppCompatActivity() {
+class TranslateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_translate)
     }
 }
